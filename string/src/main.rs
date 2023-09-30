07d4c99b516add214f8253590ce33757ed2270a4
Rabian Ikwan <rabianikwan67@gmail.com>
2023-09-30 15:26:02 +0800

    feat: chapter 9

--- string/src/main.rs
+++ string/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    // this is how to create a string
+    // kedua adalah hal yang sama
+    let data = "ayam goreng".to_string();
+    let data_ke2 = String::from("ayam goreng");
+
+    let data_ke3 = data + " " + &data_ke2;
+    println!("{}", data_ke3);
 }
